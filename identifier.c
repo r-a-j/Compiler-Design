77baee60694092ecda0be50846b689660eba738e
Raj Pawar <er.rajpawar@gmail.com>
2018-07-14 11:58:14 +0530

    Started stage 1
    taken input from user

---
+++ identifier.c
@@ -0,0 +1,25 @@
+// Created on 14-July-2018 11:53 IST
+// Created By Pawar Raj - Git Usename : r-a-j
+// Program to checking weather the entered word can be accepted as an identifier or not.
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+void main()
+{
+  // Declarations --------------------
+
+  char identifier[100],check[100];
+
+  // End of Declarations------------------
+
+  printf("Welcome to the program of checking the identifier...\n");
+  printf("Enter the identifier : ");
+  gets(identifier);
+
+  for(i=0 ; i<strlen(identifier) ; i++)
+  {
+
+  }
+}
